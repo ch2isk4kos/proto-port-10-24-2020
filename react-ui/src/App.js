dcd7782f199a1117679f4182174cf303d1986916
ch2isk4kos <ch2isk4kos@gmail.com>
2021-10-23 19:03:20 -0400

    test material appbar

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import NavigationBar from "./components/layout/NavigationBar";
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       {/*  */}
       <h1>Hello, World!</h1>
       {/*  */}
